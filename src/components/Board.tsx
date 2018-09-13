ce2987624551aaaa58516b4fcd56fa3fec4ec383
jon patrick <jjp5107@live.com>
2018-09-12 21:33:31 -0500

    Adding svg style and positioning

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -40,19 +40,19 @@ export default class Board extends React.Component<BoardProps, {}>{
 
                 <svg id="board-svg" xmlns="http://www.w3.org/2000/svg" version="1.1" width="1500" height="1500">
                     <defs>
-                        <pattern id="image-bg" x="0" y="0" height="300" width="300" patternUnits="userSpaceOnUse">
-                            <image width="1000" height="1000" ></image>
+                        <pattern id="image-bg" height="240" width="200" patternUnits="userSpaceOnUse">
+                            <image href={"http://placekitten.com/306/306"} width="300" height="300" ></image>
                         </pattern>
                     </defs>
 
 
 
                     {/* <polygon className="hex" points="100,240 200,180 200,60 100,0 0,60 0,180" fill="url('#image-bg')"></polygon> */}
-                    {this.createHexRow(1, 3)}
-                    {this.createHexRow(2, 4)}
-                    {this.createHexRow(3, 5)}
-                    {this.createHexRow(4, 4)}
-                    {this.createHexRow(5, 3)}
+                    {this.createHexRow(1, 3, this.hexWidth, 0)}
+                    {this.createHexRow(2, 4, (this.hexWidth / 2), (this.bottomY - this.bottomLeftY))}
+                    {this.createHexRow(3, 5, 0, (this.bottomY - this.bottomLeftY) * 2)}
+                    {this.createHexRow(4, 4, (this.hexWidth / 2), (this.bottomY - this.bottomLeftY) * 3)}
+                    {this.createHexRow(5, 3, this.hexWidth, (this.bottomY - this.bottomLeftY) * 4)}
 
                 </svg >
             </div>
@@ -78,25 +78,26 @@ export default class Board extends React.Component<BoardProps, {}>{
 
     // }
 
-    createHexRow(row: number, count: number) {
+    createHexRow(row: number, count: number, xOffset: number, yOffSet: number) {
 
         let hexes: any = [];
 
         for (var i = 0; i < count; i++) {
             let num = Math.floor(Math.random() * 11) + 2;
-            // let res = this.randomResource(Math.floor(Math.random() * 5) + 1);
+            let res = this.randomResource(Math.floor(Math.random() * 5) + 1);
             //Second offset column
-            let points = (this.topX + (this.hexWidth * i)) + "," + (this.topY + (this.hexHeight * row))
-                + " " + (this.upperLeftX + (this.hexWidth * i)) + "," + (this.upperLeftY + (this.hexHeight * row))
-                + " " + (this.bottomLeftX + (this.hexWidth * i)) + "," + (this.bottomLeftY + (this.hexHeight * row))
-                + " " + (this.bottomX + (this.hexWidth * i)) + "," + (this.bottomY + (this.hexHeight * row))
-                + " " + (this.bottomRightX + (this.hexWidth * i)) + "," + (this.bottomRightY + (this.hexHeight * row))
-                + " " + (this.upperRightX + (this.hexWidth * i)) + "," + (this.upperRightY + (this.hexHeight * row))
-            // let hexClass = "hex hex_" + res
+            let points = (this.topX + (this.hexWidth * i) + xOffset) + "," + (this.topY + (this.hexHeight * row) - yOffSet)
+                + " " + (this.upperLeftX + (this.hexWidth * i) + xOffset) + "," + (this.upperLeftY + (this.hexHeight * row) - yOffSet)
+                + " " + (this.bottomLeftX + (this.hexWidth * i) + xOffset) + "," + (this.bottomLeftY + (this.hexHeight * row) - yOffSet)
+                + " " + (this.bottomX + (this.hexWidth * i) + xOffset) + "," + (this.bottomY + (this.hexHeight * row) - yOffSet)
+                + " " + (this.bottomRightX + (this.hexWidth * i) + xOffset) + "," + (this.bottomRightY + (this.hexHeight * row) - yOffSet)
+                + " " + (this.upperRightX + (this.hexWidth * i) + xOffset) + "," + (this.upperRightY + (this.hexHeight * row) - yOffSet)
+            let hexClass = "hex hex_" + res
+            // let hexFill = "url(#image-bg)";
             var html = (
                 <g>
-                    <polygon points={points} className="hex" ></polygon>
-                    <text x={this.hexWidth * (i + 1) - .5 * this.hexWidth} y={this.hexHeight * (row + 1) - .5 * this.hexHeight} >{num}</text>
+                    <polygon points={points} className={hexClass} ></polygon>
+                    <text x={(this.hexWidth * (i + 1) - .5 * this.hexWidth) + xOffset} y={(this.hexHeight * (row + 1) - .5 * this.hexHeight) - yOffSet} >{num}</text>
                 </g>
                 // <polygon points={points} className="hex" ></polygon>
             )
